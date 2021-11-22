922451e2850930042cefa01bee16325b71c20724
Antonín <71491435+antoninvondrovic@users.noreply.github.com>
2021-11-22 09:45:30 +0100

    progress

--- src/main/java/cz/educanet/lights/out/domain/Game.java
+++ src/main/java/cz/educanet/lights/out/domain/Game.java
@@ -4,6 +4,8 @@ import cz.educanet.lights.out.domain.interfaces.ILightsOut;
 
 public class Game implements ILightsOut {
 
+    private boolean[][] grid = new boolean[5][5];
+    private boolean retard = true;
     int moves = 0;
 
     @Override
@@ -16,17 +18,32 @@ public class Game implements ILightsOut {
         if (false) {
             return true;
         } else {
+            if (retard) {
+                for (int i = 0; i < grid.length; i++) {
+                    for (int j = 0; j < grid[i].length; j++) {
+                        grid[i][j] = true;
+                    }
+                }
+                retard = false;
+            }
             return false;
         }
     }
 
     @Override
     public boolean[][] getGrid() {
-        return new boolean[5][5];
+        return grid;
     }
 
     @Override
     public void makeMove(int x, int y) {
+        if(grid[x][y] = false) {
+            System.out.println("false");
+            grid[x][y] = true;
+        } else if (grid[x][y] = true){
+            System.out.println("true");
+            grid[x][y] = false;
+        }
         moves++;
     }
 
